f89117f42972cf0ae80c642e9d99bb17a7a3ca66
VladislavMitkov <vladislavmitkovv@gmail.com>
2024-05-03 16:46:55 +0300

    queryParams request.

--- server.js
+++ server.js
@@ -8,11 +8,69 @@ const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 
+// DUMMY DATA
+const tasks = [
+  {
+    id: 1,
+    title: "Task 1",
+    description: "Description for Task 1",
+    priority: "low",
+    status: "todo",
+    dueDate: "2024-04-30",
+  },
+  {
+    id: 2,
+    title: "Task 2",
+    description: "Description for Task 2",
+    priority: "medium",
+    status: "in-progress",
+    dueDate: "2024-05-05",
+  },
+  {
+    id: 3,
+    title: "Task 3",
+    description: "Description for Task 3",
+    priority: "high",
+    status: "done",
+    dueDate: "2024-04-25",
+  },
+  {
+    id: 4,
+    title: "Task 4",
+    description: "Description for Task 4",
+    priority: "low",
+    status: "todo",
+    dueDate: "2024-05-10",
+  },
+  {
+    id: 5,
+    title: "Task 5",
+    description: "Description for Task 5",
+    priority: "high",
+    status: "in-progress",
+    dueDate: "2024-04-28",
+  },
+];
+
 // Define a route handler for the root URL ("/")
 app.get("/", (req, res) => {
   const payload = req.body;
   console.log(payload);
   res.send("Welcome to Planner App Backend");
+});
+
+app.get("/tasks", (req, res) => {
+  const filterTasks = (task) => {
+    //we iterates over each KEY in the query Params
+    for (const key in req.query) {
+      if (task[key] !== req.query[key]) {
+        return false;
+      }
+    }
+    return true;
+  };
+  const filteredTasks = tasks.filter(filterTasks);
+  res.send(filteredTasks);
 });
 
 //get the full list of your tasks
@@ -23,8 +81,9 @@ app.get("/tasks", (req, res) => {
 
 //get a single task by Id
 app.get("/tasks/:id", (req, res) => {
-  console.log("Task by id");
-  res.send("get");
+  const taskId = req.params.id;
+  console.log("Task by id", taskId);
+  res.send("Task with id: " + taskId);
 });
 
 //make a POST request to add task
